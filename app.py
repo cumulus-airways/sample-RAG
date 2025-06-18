40e19e3d7f5c9e0a50fbf08962625f334ab23db1
Keerthana1695 <keerthana161995@gmail.com>
2025-06-18 11:12:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 #     return os.path.join(target_dir, "document .docx")  # adjust if needed
 # repo_url = "https://github.com/Keerthana1695/sample-RAG.git"
 # doc_path = clone_repo(repo_url)
-file_path = ["./document.txt"]
+file_path = ("./document.txt")
 loader = TextLoader(file_path)
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
